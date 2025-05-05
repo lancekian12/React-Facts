bf8ea8b6c3931d71bb8d51f61cc98cc2c78a08af
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-05 21:28:42 +0800

    using component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 
 import './App.css'
+import TemporaryName from './components/TemporaryName'
 
 function App() {
 
   return (
     <>
-      <main>
-        <img src="react-logo.png" width="40" />
-        <h1>Fun Facts About React</h1>
-        <ul>
-          <li>Was First Released in 2013</li>
-          <li>Was Originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on Github</li>
-          <li>Is maintained by Meta</li>
-          <li>Power thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
+      <TemporaryName />
     </>
   )
 }
---
+++ src/components/TemporaryName.jsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+const TemporaryName = () => {
+    return (
+        <main>
+            <img src="react-logo.png" width="40" />
+            <h1>Fun Facts About React</h1>
+            <ul>
+                <li>Was First Released in 2013</li>
+                <li>Was Originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Power thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>)
+}
+
+export default TemporaryName
