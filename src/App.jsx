257deb003716d93f75ee65cd5bc8fca15ac24b9c
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-05 21:18:22 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <img src="react-logo.png" width="40" />
+        <h1>Fun Facts About React</h1>
+        <ul>
+          <li>Was First Released in 2013</li>
+          <li>Was Originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by Meta</li>
+          <li>Power thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export default App
